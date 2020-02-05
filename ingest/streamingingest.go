a2ad45ea72484b24b0c50c106c250e57b680615a
Daniel Dubovski <dadubovs@microsoft.com>
2020-02-05 18:17:03 +0200

    wip streaming ingestion

--- ingest/streamingingest.go
+++ ingest/streamingingest.go
@@ -1,26 +1,46 @@
 package ingest
 
 import (
-	"azure-kusto-go/data"
+	"github.com/Azure/azure-kusto-go/data"
 )
+
+type streamPacket struct {
+	data chan []byte
+	props IngestionProperties
+}
 
 type StreamingIngestClient struct {
 	client          *data.Client
 	resourceManager resourceManager
+	out chan[1] streamPacket
 }
 
-func NewStreamingIngestClient(dmEndpoint string, authorization data.Authorization) *IngestClient {
+
+func (ic StreamingIngestClient) streamToKusto() {
+	for sp := range ic.out{
+		ic.client.Query()
+	}
+}
+
+func NewStreaming(dmEndpoint string, authorization data.Authorization) *StreamingIngestClient {
 	dmClient, _ := data.New(dmEndpoint, authorization);
-	return &IngestClient{
+	sic := &StreamingIngestClient{
 		client: dmClient,
 		resourceManager: resourceManager{
 			client:    dmClient,
 			resources: nil,
 		},
+		out: make(chan [1] streamPacket),
 	}
+
+	go sic.streamToKusto()
+
+	return sic
 }
 
 
-func (ic StreamingIngestClient) IngestFromStream(stream chan []byte, options StreamSourceOptions) {
+func (ic StreamingIngestClient) IngestFromStream(in chan []byte, properties IngestionProperties, options map[string]string) {
+	for b := range in{
 
+	}
 }
