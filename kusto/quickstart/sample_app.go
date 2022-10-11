37ea9be6aa642a9d2a0153dca718e3dbecd9e6e3
t-ronmoneta <t-ronmoneta@microsoft.com>
2022-10-11 17:02:50 +0300

    module reorganizing

--- kusto/quickstart/sample_app.go
+++ kusto/quickstart/sample_app.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/Azure/azure-kusto-go/kusto"
 	"github.com/Azure/azure-kusto-go/kusto/ingest"
+	"github.com/Azure/azure-kusto-go/kusto/quickstart/utils"
+	"github.com/Azure/azure-kusto-go/kusto/quickstart/utils/authentication"
 	"io/ioutil"
 	"os"
 )
@@ -15,31 +18,40 @@ const (
 	blobSource      SourceType = "blobSource"
 )
 
+type AuthenticationModeOptions string
+
+const (
+	UserPrompt      AuthenticationModeOptions = "UserPrompt"
+	ManagedIdentity AuthenticationModeOptions = "ManagedIdentity"
+	AppKey          AuthenticationModeOptions = "AppKey"
+	AppCertificate  AuthenticationModeOptions = "AppCertificate"
+)
+
 type ConfigJson struct {
-	UseExistingTable bool         `json:"useExistingTable"`
-	DatabaseName     string       `json:"databaseName"`
-	TableName        string       `json:"tableName"`
-	TableSchema      string       `json:"tableSchema"`
-	KustoUri         string       `json:"kustoUri"`
-	IngestUri        string       `json:"ingestUri"`
-	DataToIngest     []ConfigData `json:"data"`
-	AlterTable       bool         `json:"alterTable"`
-	QueryData        bool         `json:"queryData"`
-	IngestData       bool         `json:"ingestData"`
-	//AuthenticationMode AuthenticationModeOptions  `json:"authenticationMode"`
-	WaitForUser          bool   `json:"waitForUser"`
-	IgnoreFirstRecord    bool   `json:"ignoreFirstRecord"`
-	WaitForIngestSeconds int    `json:"waitForIngestSeconds"`
-	BatchingPolicy       string `json:"batchingPolicy"`
+	UseExistingTable     bool                      `json:"useExistingTable"`
+	DatabaseName         string                    `json:"databaseName"`
+	TableName            string                    `json:"tableName"`
+	TableSchema          string                    `json:"tableSchema"`
+	KustoUri             string                    `json:"kustoUri"`
+	IngestUri            string                    `json:"ingestUri"`
+	DataToIngest         []ConfigData              `json:"data"`
+	AlterTable           bool                      `json:"alterTable"`
+	QueryData            bool                      `json:"queryData"`
+	IngestData           bool                      `json:"ingestData"`
+	AuthenticationMode   AuthenticationModeOptions `json:"authenticationMode"`
+	WaitForUser          bool                      `json:"waitForUser"`
+	IgnoreFirstRecord    bool                      `json:"ignoreFirstRecord"`
+	WaitForIngestSeconds int                       `json:"waitForIngestSeconds"`
+	BatchingPolicy       string                    `json:"batchingPolicy"`
 }
 
 type ConfigData struct {
-	SourceType         SourceType        `json:"sourceType"`
-	DataSourceUri      string            `json:"dataSourceUri"`
-	DataFormat         ingest.DataFormat `json:"format"`
-	UseExistingMapping bool              `json:"useExistingMapping"`
-	MappingName        string            `json:"mappingName"`
-	MappingValue       string            `json:"mappingValue"`
+	SourceType    SourceType `json:"sourceType"`
+	DataSourceUri string     `json:"dataSourceUri"`
+	//DataFormat         ingest.DataFormat `json:"format"`
+	UseExistingMapping bool   `json:"useExistingMapping"`
+	MappingName        string `json:"mappingName"`
+	MappingValue       string `json:"mappingValue"`
 }
 
 func load_configs(configFileName string) ConfigJson {
@@ -66,10 +78,51 @@ func load_configs(configFileName string) ConfigJson {
 	return config
 }
 
+func waitForUserToProceed(promptMsg string, step int, waitForUser bool) {
+	fmt.Println()
+	fmt.Printf("\nStep %d: %s", step, promptMsg)
+	step++
+	if waitForUser {
+		fmt.Println("Press ENTER to proceed with this operation...")
+		var c string
+		_, _ = fmt.Scanln(&c)
+	}
+
+}
+
 func main() {
 	fmt.Println("Kusto sample app is starting...")
+	var step = 1
 	const configFileName = "kusto/quickstart/kusto_sample_config.json"
 	var config = load_configs(configFileName)
-	fmt.Print(config)
+
+	if config.AuthenticationMode == UserPrompt {
+		waitForUserToProceed("You will be prompted *twice* for credentials during this script. Please return to the console after authenticating.", step, config.WaitForUser)
+	}
+
+	var kustoConnectionString = authentication.generate_connection_string(config.KustoUri, config.AuthenticationMode)
+	kustoClient, err := kusto.New(config.KustoUri, kustoConnectionString)
+	if err != nil {
+		utils.errorHandler("Couldn't create client. Please validate your URIs in the configuration file.", err)
+	}
+	defer func(client *kusto.Client) {
+		err := client.Close()
+		if err != nil {
+			utils.errorHandler("Couldn't close client.", err)
+		}
+	}(kustoClient)
+
+	ingestClient, err := ingest.New(kustoClient, config.DatabaseName, config.TableName)
+	if err != nil {
+		utils.errorHandler("Couldn't create client. Please validate your URIs in the configuration file.", err)
+	}
+	// Be sure to close the ingestor when you're done. (Error handling omitted for brevity.)
+	defer func(ingestClient *ingest.Ingestion) {
+		err := ingestClient.Close()
+		if err != nil {
+			utils.errorHandler("Couldn't close client.", err)
+		}
+	}(ingestClient)
+
 	fmt.Println("\nKusto sample app done")
 }
--- kusto/quickstart/utils/queries/queries.go
+++ kusto/quickstart/utils/queries/queries.go
@@ -1,4 +1,4 @@
-package utils
+package queries
 
 /**
 Queries module of Utils - in charge of querying the data - either with management queries, or data queries
