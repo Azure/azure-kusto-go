3c8188d349cdfc94b029ec639a336d09b8842f36
AsafMah <asafmahlev@microsoft.com>
2022-12-01 15:25:02 +0200

    Fixed leaks (#143)

--- kusto/test/etoe/etoe_test.go
+++ kusto/test/etoe/etoe_test.go
@@ -1735,6 +1735,7 @@ func createIngestionTableWithDBAndScheme(t *testing.T, client *kusto.Client, dat
 	t.Cleanup(func() {
 		t.Logf("Dropping ingestion table %s", tableName)
 		_ = executeCommands(client, database, dropUnsafe)
+		t.Logf("Dropped ingestion table %s", tableName)
 	})
 
 	return executeCommands(client, database, dropUnsafe, createUnsafe, addMappingUnsafe, clearStreamingCacheStatement)
