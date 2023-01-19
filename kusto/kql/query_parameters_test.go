620266a91f3d9bb123319f5ac31956ce0899bbf7
t-ronmoneta <t-ronmoneta@microsoft.com>
2023-01-19 16:19:08 +0200

    q_param test fix

--- kusto/kql/query_parameters_test.go
+++ kusto/kql/query_parameters_test.go
@@ -14,7 +14,7 @@ func TestQueryParameters(t *testing.T) {
 		expected string
 	}{
 		{"Test empty", NewStatementBuilder(""), NewStatementQueryParameters(), "\n"},
-		{"Test add literal", NewStatementBuilder(""), NewStatementQueryParameters().AddLiteral("foo", "string", "bar"), "declare query_parameters(foo:string=bar);\n"},
+		{"Test add literal", NewStatementBuilder(""), NewStatementQueryParameters().AddLiteral("foo", "string", "bar"), "declare query_parameters(foo:string);\n"},
 		{"Test add identifiers", // test might fail at times due to the pseudo-random nature of map that will sometimes change the order of the declaration string.
 			NewStatementBuilder("").
 				AddDatabase("database").AddLiteral(".").
@@ -26,7 +26,7 @@ func TestQueryParameters(t *testing.T) {
 				AddLiteral("table", "string", "_bar").
 				AddLiteral("column", "string", "_baz").
 				AddLiteral("function", "string", "func_"),
-			"declare query_parameters(database:string=foo_1, table:string=_bar, column:string=_baz, function:string=func_);\ndatabase(\"database\").table | where column == function() ;"},
+			"declare query_parameters(database:string, table:string, column:string, function:string);\ndatabase(\"database\").table | where column == function() ;"},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
