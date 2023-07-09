161513132e9828ad0e2a0d5946f7e7e1ff9e6bb7
AsafMah <asafmahlev@microsoft.com>
2023-07-09 16:03:33 +0300

    Fixed

--- kusto/reader.go
+++ kusto/reader.go
@@ -127,7 +127,7 @@ func (r *RowIterator) start() chan struct{} {
 		defer closeDone() // Catchall
 		logger.Info().Msg("starting row iterator goroutine")
 
-		rowBuffer := make([]value.Values, 0, 1000)
+		rowBuffer := make([]send, 0, 1000)
 		gotColumns := false
 
 		for {
@@ -146,28 +146,32 @@ func (r *RowIterator) start() chan struct{} {
 					return
 				}
 				if !gotColumns {
-					rowBuffer = append(rowBuffer, sent.inRows...)
+					rowBuffer = append(rowBuffer, sent)
+					sent.done()
+					continue
 				}
 				if len(rowBuffer) > 0 {
 					logger.Info().Msg("sending buffered rows")
-					if sent.inRows != nil {
-						logger.Info().Msg("starting buffered rows")
-						for k, values := range sent.inRows {
-							select {
-							case <-r.ctx.Done():
-							case r.rows <- Row{Values: values, Replace: k == 0 && sent.inTableFragmentType == "DataReplace"}:
+					for _, s := range rowBuffer {
+						if s.inRows != nil {
+							logger.Info().Msg("starting buffered rows")
+							for k, values := range s.inRows {
+								select {
+								case <-r.ctx.Done():
+								case r.rows <- Row{Values: values, Replace: k == 0 && s.inTableFragmentType == "DataReplace"}:
+								}
 							}
+							logger.Info().Msg("sent buffered rows")
 						}
-						logger.Info().Msg("sent buffered rows")
-					}
 
-					logger.Info().Msg("sending buffered row errors")
-					if sent.inRowErrors != nil {
-						for _, e := range sent.inRowErrors {
-							e := e // capture so we can send reference
-							select {
-							case <-r.ctx.Done():
-							case r.rows <- Row{Error: &e}:
+						logger.Info().Msg("sending buffered row errors")
+						if s.inRowErrors != nil {
+							for _, e := range s.inRowErrors {
+								e := e // capture so we can send reference
+								select {
+								case <-r.ctx.Done():
+								case r.rows <- Row{Error: &e}:
+								}
 							}
 						}
 					}
