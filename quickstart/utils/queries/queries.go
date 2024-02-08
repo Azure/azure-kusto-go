b1355453b989324febc87f64293050f17d06eda3
asafmahlev <asafmahlev@microsoft.com>
2024-02-08 11:02:37 +0200

    Updated sample app

--- quickstart/utils/queries/queries.go
+++ quickstart/utils/queries/queries.go
@@ -5,40 +5,35 @@ import (
 	"context"
 	"fmt"
 	"github.com/Azure/azure-kusto-go/azkustodata"
-	kustoErrors "github.com/Azure/azure-kusto-go/azkustodata/errors"
-	"github.com/Azure/azure-kusto-go/azkustodata/table"
+	"github.com/Azure/azure-kusto-go/azkustodata/query"
 	"strings"
 )
 
 // ExecuteCommand Executes a Command using a premade client
 func ExecuteCommand(kustoClient *azkustodata.Client, databaseName string, command azkustodata.Statement, options ...azkustodata.QueryOption) {
 	ctx := context.Background()
-	var iter *azkustodata.RowIterator
+	var result query.FullDataset
 	var err error
 	if strings.HasPrefix(command.String(), ".") {
-		iter, err = kustoClient.Mgmt(ctx, databaseName, command)
+		result, err = kustoClient.Mgmt(ctx, databaseName, command)
 	} else {
-		iter, err = kustoClient.Query(ctx, databaseName, command, options...)
+		result, err = kustoClient.Query(ctx, databaseName, command, options...)
 	}
 
 	if err != nil {
 		panic(fmt.Sprintf("Command execution failed: '%s'\n", err.Error()))
 	}
-	defer iter.Stop()
 
-	// .DoOnRowOrError() will call the function for every row in the table and throw an error if occurred.
-	err = iter.DoOnRowOrError(
-		func(row *table.Row, err *kustoErrors.Error) error {
-			if err != nil {
-				panic(fmt.Sprintf("Failed handling row: '%s'\n", err.Error()))
-			}
-			if row.Replace {
-				fmt.Println("---") // Replace flag indicates that the query result should be cleared and replaced with this row
-			}
-			fmt.Println(row) // As a convenience, printing a *table.Row will output csv
-			return nil
-		},
-	)
+	for _, table := range result.Tables() {
+		if !table.IsPrimaryResult() {
+			continue
+		}
+
+		for _, row := range table.Rows() {
+			fmt.Println(row)
+		}
+	}
+
 	if err != nil {
 		panic(fmt.Sprintf("Failed printing the results: '%s'\n", err.Error()))
 	}
