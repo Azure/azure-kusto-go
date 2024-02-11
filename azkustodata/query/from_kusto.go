8e6ded40eaa2448f6d8801e7c5e738f5bba0e532
asafmahlev <asafmahlev@microsoft.com>
2024-02-11 08:54:14 +0200

    cache decodeToStruct result

--- azkustodata/query/from_kusto.go
+++ azkustodata/query/from_kusto.go
@@ -1,12 +1,17 @@
 package query
 
 import (
-	"fmt"
+	kustoErrors "github.com/Azure/azure-kusto-go/azkustodata/errors"
+	"github.com/Azure/azure-kusto-go/azkustodata/value"
 	"reflect"
 	"strings"
-
-	"github.com/Azure/azure-kusto-go/azkustodata/value"
 )
+
+type fieldMap struct {
+	colNameToFieldName map[string]string
+}
+
+var typeMapper = map[reflect.Type]fieldMap{}
 
 // decodeToStruct takes a list of columns and a row to decode into "p" which will be a pointer
 // to a struct (enforce in the decoder).
@@ -23,28 +28,27 @@ func decodeToStruct(cols []Column, row value.Values, p interface{}) error {
 	return nil
 }
 
-// fields represents the fields inside a struct.
-type fields struct {
-	colNameToFieldName map[string]string
-}
-
 // newFields takes in the Columns from our row and the reflect.Type of our *struct.
-func newFields(ptr reflect.Type) fields {
-	nFields := fields{colNameToFieldName: map[string]string{}}
-	for i := 0; i < ptr.Elem().NumField(); i++ {
-		field := ptr.Elem().Field(i)
-		if tag := field.Tag.Get("kusto"); strings.TrimSpace(tag) != "" {
-			nFields.colNameToFieldName[tag] = field.Name
-		} else {
-			nFields.colNameToFieldName[field.Name] = field.Name
+func newFields(ptr reflect.Type) fieldMap {
+	if f, ok := typeMapper[ptr]; ok {
+		return f
+	} else {
+		nFields := fieldMap{colNameToFieldName: make(map[string]string, ptr.Elem().NumField())}
+		for i := 0; i < ptr.Elem().NumField(); i++ {
+			field := ptr.Elem().Field(i)
+			if tag := field.Tag.Get("kusto"); strings.TrimSpace(tag) != "" {
+				nFields.colNameToFieldName[tag] = field.Name
+			} else {
+				nFields.colNameToFieldName[field.Name] = field.Name
+			}
 		}
+		typeMapper[ptr] = nFields
+		return nFields
 	}
-
-	return nFields
 }
 
 // convert converts a KustoValue that is for Column col into "v" reflect.Value with reflect.Type "t".
-func (f fields) convert(col Column, k value.Kusto, v reflect.Value) error {
+func (f fieldMap) convert(col Column, k value.Kusto, v reflect.Value) error {
 	fieldName, ok := f.colNameToFieldName[col.Name()]
 	if !ok {
 		return nil
@@ -56,7 +60,7 @@ func (f fields) convert(col Column, k value.Kusto, v reflect.Value) error {
 
 	err := k.Convert(v.Elem().FieldByName(fieldName))
 	if err != nil {
-		return fmt.Errorf("column %s could not store in struct.%s: %s", col.Name(), fieldName, err.Error())
+		return kustoErrors.ES(kustoErrors.OpTableAccess, kustoErrors.KWrongColumnType, "column %s could not store in struct.%s: %s", col.Name(), fieldName, err.Error())
 	}
 
 	return nil
