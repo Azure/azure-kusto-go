4eced919c63e16072102b647d69c16bb0ac163a7
asafmahlev <asafmahlev@microsoft.com>
2024-06-25 16:32:59 +0300

    removed indirection

--- azkustodata/query/v2/iterative_table.go
+++ azkustodata/query/v2/iterative_table.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Azure/azure-kusto-go/azkustodata/errors"
 	"github.com/Azure/azure-kusto-go/azkustodata/query"
 	"sync"
+	"sync/atomic"
 )
 
 // iterativeTable represents a table that is streamed from the service.
@@ -12,40 +13,43 @@ import (
 // The rows are received from the service via the rawRows channel, and are parsed and sent to the rows channel.
 type iterativeTable struct {
 	query.BaseTable
-	lock     sync.RWMutex
-	rawRows  chan []query.Row
-	rows     chan query.RowResult
-	rowCount int
-	skip     bool
-	ctx      context.Context
+	rows            chan query.RowResult
+	rowCount        atomic.Uint32
+	skip            atomic.Bool
+	ctx             context.Context
+	reportSkipError sync.Once
 }
 
 func (t *iterativeTable) addRawRows(rows []query.Row) {
-	t.rawRows <- rows
+	for _, row := range rows {
+		if t.Skip() {
+			t.reportSkipError.Do(func() {
+				t.reportError(errors.ES(t.Op(), errors.KInternal, skipError))
+			})
+			return
+		} else {
+			if !t.reportRow(row) {
+				return
+			}
+		}
+		t.rowCount.Add(1)
+	}
 }
 
 func (t *iterativeTable) RowCount() int {
-	t.lock.RLock()
-	defer t.lock.RUnlock()
-	return t.rowCount
+	return int(t.rowCount.Load())
 }
 
 func (t *iterativeTable) setRowCount(rowCount int) {
-	t.lock.Lock()
-	defer t.lock.Unlock()
-	t.rowCount = rowCount
+	t.rowCount.Store(uint32(rowCount))
 }
 
 func (t *iterativeTable) Skip() bool {
-	t.lock.RLock()
-	defer t.lock.RUnlock()
-	return t.skip
+	return t.skip.Load()
 }
 
 func (t *iterativeTable) setSkip(skip bool) {
-	t.lock.Lock()
-	defer t.lock.Unlock()
-	t.skip = skip
+	t.skip.Store(skip)
 }
 
 func NewIterativeTable(dataset *iterativeDataset, th TableHeader) (query.IterativeTable, error) {
@@ -57,20 +61,19 @@ func NewIterativeTable(dataset *iterativeDataset, th TableHeader) (query.Iterati
 	t := &iterativeTable{
 		BaseTable: baseTable,
 		ctx:       dataset.Context(),
-		rawRows:   make(chan []query.Row, dataset.fragmentCapacity),
 		rows:      make(chan query.RowResult, dataset.rowCapacity),
 	}
-
-	go t.readRows()
 
 	return t, nil
 }
 
-func (t *iterativeTable) finishTable(errs []OneApiError) {
-	if errs != nil {
-		t.rows <- query.RowResultError(combineOneApiErrors(errs))
+func (t *iterativeTable) finishTable(errs []OneApiError, cancelError error) {
+	if cancelError != nil {
+		t.reportError(cancelError)
+	} else if errs != nil {
+		t.reportError(combineOneApiErrors(errs))
 	}
-	close(t.rawRows)
+	close(t.rows)
 }
 
 func (t *iterativeTable) reportRow(row query.Row) bool {
@@ -93,25 +96,6 @@ func (t *iterativeTable) reportError(err error) bool {
 
 const skipError = "skipping row"
 
-func (t *iterativeTable) readRows() {
-	defer close(t.rows)
-
-	for rows := range t.rawRows {
-		for _, row := range rows {
-			if t.Skip() {
-				if !t.reportError(errors.ES(t.Op(), errors.KInternal, skipError)) {
-					return
-				}
-			} else {
-				if !t.reportRow(row) {
-					return
-				}
-			}
-			t.rowCount++
-		}
-	}
-}
-
 func (t *iterativeTable) Rows() <-chan query.RowResult {
 	return t.rows
 }
@@ -130,9 +114,7 @@ func (t *iterativeTable) SkipToEnd() []error {
 }
 
 func (t *iterativeTable) ToTable() (query.Table, error) {
-	t.lock.RLock()
-	defer t.lock.RUnlock()
-	if t.skip {
+	if t.Skip() {
 		return nil, errors.ES(t.Op(), errors.KInternal, "table is already skipped to the end")
 	}
 
