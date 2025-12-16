6878db2e8b2537073131bf26b609a117fb3f2f49
Noam Cohen <noam@noam.me>
2025-12-16 19:27:28 +0200

    make `putQueued` return an error

--- azkustoingest/result.go
+++ azkustoingest/result.go
@@ -32,11 +32,11 @@ func (r *Result) putProps(props properties.All) {
 }
 
 // putQueued sets the initial success status depending on status reporting state
-func (r *Result) putQueued(ctx context.Context, i *Ingestion) {
+func (r *Result) putQueued(ctx context.Context, i *Ingestion) error {
 	// If not checking status, just return queued
 	if !r.reportToTable {
 		r.record.Status = Queued
-		return
+		return nil
 	}
 
 	// Get table URI
@@ -45,14 +45,14 @@ func (r *Result) putQueued(ctx context.Context, i *Ingestion) {
 		r.record.Status = StatusRetrievalFailed
 		r.record.FailureStatus = Permanent
 		r.record.Details = "Failed getting status table URI: " + err.Error()
-		return
+		return &r.record
 	}
 
 	if len(tableResources) == 0 {
 		r.record.Status = StatusRetrievalFailed
 		r.record.FailureStatus = Permanent
 		r.record.Details = "Ingestion resources do not include a status table URI: " + err.Error()
-		return
+		return &r.record
 	}
 
 	// create a table client
@@ -61,7 +61,7 @@ func (r *Result) putQueued(ctx context.Context, i *Ingestion) {
 		r.record.Status = StatusRetrievalFailed
 		r.record.FailureStatus = Permanent
 		r.record.Details = "Failed Creating a Status Table client: " + err.Error()
-		return
+		return &r.record
 	}
 
 	// StreamIngest initial record
@@ -71,10 +71,11 @@ func (r *Result) putQueued(ctx context.Context, i *Ingestion) {
 		r.record.Status = StatusRetrievalFailed
 		r.record.FailureStatus = Permanent
 		r.record.Details = "Failed writing initial status record: " + err.Error()
-		return
+		return &r.record
 	}
 
 	r.tableClient = client
+	return nil
 }
 
 // Wait returns a channel that can be checked for ingestion results.
